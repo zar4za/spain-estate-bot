fdd9efdc994d8ae9b5e34b5ec6befa520ec4b3c8
zar4za <zar4z4@yandex.ru>
2023-03-07 00:04:51 +0300

    fix: catch json exception
    Обработка исключения парсинга конфигурации. Перенесен стандартный конфиг в функцию.

--- src/config.py
+++ src/config.py
@@ -1,15 +1,20 @@
 import json
+from json import JSONDecodeError
 
 
-def load_config(path: str):
+def load_config(path: str = "../appsettings.json"):
     dev_path = path.replace(".json", ".development.json")
     paths = [dev_path, path]
 
     for p in paths:
         try:
             with open(p, "r") as file:
-                return json.load(file)
-        except FileNotFoundError:
+                config = json.load(file)
+                return config
+        except JSONDecodeError:
+            raise FileNotFoundError(f"A configuration file named {p.split('/')[-1]} can not be parsed.")
+        finally:
             continue
+
     else:
-        raise FileNotFoundError("A configuration file named appsettings.json was not found")
+        raise FileNotFoundError(f"A configuration file named {path.split('/')[-1]} was not found")
