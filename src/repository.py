f2a9c724441f058ca1417163f77288d333a806cb
zar4za <zar4z4@yandex.ru>
2023-03-07 14:05:38 +0300

    fix: changed column name in sql
    Изменено название колонки id пользователя.

--- src/repository.py
+++ src/repository.py
@@ -14,28 +14,31 @@ class Repository:
 
     async def connect(self):
         for i in range(3):
+            print(f"Trying to connect to Postgres. Attempt {i + 1}")
             try:
                 self.__connection = psycopg2.connect(
                     dbname=self.__dbname,
-                    username=self.__user,
+                    user=self.__user,
                     password=self.__password,
                     host=self.__host
                 )
+                print("Connected to Postgres")
                 break
             except:
+                print("Cannot connect to Postgres")
                 await sleep(5)
         else:
             raise ConnectionError("Can not connect to db after 3 attempts")
 
     def is_user_registered(self, userid) -> bool:
         cursor = self.__connection.cursor()
-        cursor.execute("SELECT * FROM users WHERE id = %s", userid)
+        cursor.execute("SELECT * FROM users WHERE userid = %s", (userid,))
         user = cursor.fetchone()
         return user is not None
 
     def is_trial_used(self, userid) -> bool:
         cursor = self.__connection.cursor()
-        cursor.execute("SELECT * FROM users WHERE id = %s", userid)
+        cursor.execute("SELECT * FROM users WHERE userid = %s", (userid,))
         user = cursor.fetchone()
 
         if user is None:
@@ -46,35 +49,40 @@ class Repository:
     def insert_user(self, user):
         cursor = self.__connection.cursor()
         cursor.execute(
-            "INSERT INTO users (id, trial, valid_until) "
+            "INSERT INTO users (userid, trial, valid_until) "
             "VALUES (%s, %s, %s)",
             (user.userid, False, None)
         )
         self.__connection.commit()
+        print(f"DB: Registered new user with id {user.userid}.")
 
-    def activate_trial(self, userid):
+    def activate_trial(self, userid) -> datetime.datetime:
         expiration = datetime.datetime.utcnow()
         expiration += datetime.timedelta(days=7)
         cursor = self.__connection.cursor()
         cursor.execute(
             "UPDATE users "
-            "SET trial = TRUE, valid_until = %s"
-            "WHERE id = %s",
+            "SET trial = TRUE, valid_until = %s "
+            "WHERE userid = %s",
             (expiration, userid)
         )
         self.__connection.commit()
+        print(f"DB: Activated trial for user with id {userid}. Expires on {expiration}.")
+        return expiration
 
-    def activate_subscription(self, userid, days):
+    def activate_subscription(self, userid, days) -> datetime.datetime:
         expiration = datetime.datetime.utcnow()
         expiration += datetime.timedelta(days=days)
         cursor = self.__connection.cursor()
         cursor.execute(
             "UPDATE users "
             "SET valid_until = %s"
-            "WHERE id = %s",
+            "WHERE userid = %s",
             (expiration, userid)
         )
         self.__connection.commit()
+        print(f"DB: Activated subscription for user with id {userid}. Expires on {expiration}.")
+        return expiration
 
     def insert_many_articles(self, articles: list):
         cursor = self.__connection.cursor()
@@ -82,3 +90,6 @@ class Repository:
         self.__connection.commit()
 
 
+class User:
+    def __init__(self, userid):
+        self.userid = userid
