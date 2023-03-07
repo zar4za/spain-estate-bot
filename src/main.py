05cda3af224fd261e47b929dc06ddeef113487a8
zar4za <zar4z4@yandex.ru>
2023-03-07 14:06:25 +0300

    feat!: tgbot use repository
    Теперь бот использует репозиторий пользователей.

--- src/main.py
+++ src/main.py
@@ -1,13 +1,16 @@
 import asyncio
 from asyncio import sleep
 
+from src.repository import Repository
 from tg import TgBot
 from config import load_config
 
 
 async def main():
     config = load_config()
-    bot = TgBot(config)
+    repo = Repository(config)
+    bot = TgBot(config, repo)
+    await repo.connect()
     await bot.start()
     await sleep(60)
     # sleep method is used while nothing is here to prevent app from closing
