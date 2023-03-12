c06af9cb3b476d10fbf122166c3acc491d17fbb9
zar4za <zar4z4@yandex.ru>
2023-03-12 14:33:18 +0300

    feat: logger for config.py
    Настройка логгирования в config.py

--- src/config.py
+++ src/config.py
@@ -1,5 +1,8 @@
 import json
+import logging
 from json import JSONDecodeError
+
+logger = logging.getLogger(__name__)
 
 
 def load_config(path: str = "../appsettings.json"):
@@ -10,11 +13,12 @@ def load_config(path: str = "../appsettings.json"):
         try:
             with open(p, "r") as file:
                 config = json.load(file)
-                print(f"Loaded config {p.split('/')[-1]}")
+                logger.info(f"Loaded configuration from file {p.split('/')[-1]}.")
                 return config
         except JSONDecodeError:
-            raise FileNotFoundError(f"A configuration file named {p.split('/')[-1]} can not be parsed.")
+            raise FileNotFoundError(f"Configuration file named {p.split('/')[-1]} can not be parsed.")
         except:
+            logger.warning(f"Configuration file {p.split('/')[-1]} was not found, trying next one.")
             continue
     else:
-        raise FileNotFoundError(f"A configuration file named {path.split('/')[-1]} was not found")
+        raise FileNotFoundError(f"Configuration file named {path.split('/')[-1]} was not found.")
